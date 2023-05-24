6676238e81a025b81e5fe036d03a21034d07ae71
kouki <e205702@ie.u-ryukyu.ac.jp>
2023-05-25 01:18:28 +0900

    #変更

--- step2/transformation/scaling.py
+++ step2/transformation/scaling.py
@@ -2,10 +2,4 @@ import cv2
 import numpy as np
 
 img = cv2.imread('/Users/kouki/Image_practice/sample/frame/sample_video1_gray_0100.jpg')
-
-while True:
-    cv2.imshow("scale",img)
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
-
-cv2.destroyWindow()
+res = cv2.resize(img,None,fx=2,fy=2,interpolation=cv2.INTER_CUBIC)
