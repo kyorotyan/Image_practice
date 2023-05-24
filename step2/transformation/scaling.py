2aec33036bc7e8f22bf1c8f30e35e91798b926f7
kouki <e205702@ie.u-ryukyu.ac.jp>
2023-05-25 08:25:38 +0900

    ##image scaling

--- step2/transformation/scaling.py
+++ step2/transformation/scaling.py
@@ -2,4 +2,13 @@ import cv2
 import numpy as np
 
 img = cv2.imread('/Users/kouki/Image_practice/sample/frame/sample_video1_gray_0100.jpg')
-res = cv2.resize(img,None,fx=2,fy=2,interpolation=cv2.INTER_CUBIC)
+res = cv2.resize(img,None,fx=11,fy=9,interpolation=cv2.INTER_CUBIC)
+
+cv2.imshow("Resize",res)
+
+k = cv2.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv2.destroyAllWindows()
+elif k == ord('s'): # wait for 's' key to save and exit
+    cv2.imwrite('messigray.png',img)
+    cv2.destroyAllWindows()
